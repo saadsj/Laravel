a42b16fc35923d40f1374e42840849079de9f62b
saadsj <47307940+saadsj@users.noreply.github.com>
2019-03-09 03:59:29 -0500

    Final Commit

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
 });
